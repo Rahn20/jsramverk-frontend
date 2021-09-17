7a4f63e0b650bb25d35d198b66ed3f1c42bf7468
unknown <ranimhanna@hotmail.com>
2021-09-17 22:42:39 +0100

    cleaned up the file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CkeditorComponent } from './ckeditor/ckeditor.component';
 
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
     declarations: [
@@ -20,6 +20,7 @@ import { CkeditorComponent } from './ckeditor/ckeditor.component';
         AppRoutingModule,
         FormsModule,
         CKEditorModule,
+        HttpClientModule
     ],
     providers: [],
     bootstrap: [AppComponent]
