53d49be1363fc8bb2aac05062df425e28654176c
unknown <ranimhanna@hotmail.com>
2021-11-06 03:39:31 +0100

    Added SocketIo module/config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,15 @@ import { AppComponent } from './app.component';
 import { CkeditorComponent } from './ckeditor/ckeditor.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+//const config: SocketIoConfig = { url: 'http://localhost:1337', options: {}};
+const config: SocketIoConfig = { 
+    url: 'https://jsramverk-editor-rahn20.azurewebsites.net', 
+    options: {
+        transports: ['websocket'],
+    },
+};
 
 @NgModule({
     declarations: [
@@ -21,6 +30,7 @@ import { HttpClientModule } from '@angular/common/http';
         FormsModule,
         CKEditorModule,
         HttpClientModule,
+        SocketIoModule.forRoot(config)
     ],
     providers: [],
     bootstrap: [AppComponent]
