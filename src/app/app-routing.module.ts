601f45baa12ea9c8a8b0955c3c00a22f13b0d08c
unknown <ranimhanna@hotmail.com>
2021-11-15 05:41:08 +0000

    Added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: '',
+        redirectTo: '/',
+        pathMatch: 'full'
+    },
+];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [RouterModule.forRoot(routes)], // { enableTracing: true } for debugging purposes only
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
