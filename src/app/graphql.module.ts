0eab4ab9a349b44aaf6d09535bdae4e95278b47d
unknown <ranimhanna@hotmail.com>
2021-12-30 03:56:38 +0000

    Changed uri

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -4,6 +4,7 @@ import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
 import {HttpLink} from 'apollo-angular/http';
 
 const uri = 'https://jsramverk-editor-rahn20.azurewebsites.net/me-api/graphql'; // <-- add the URL of the GraphQL server here
+//const uri = 'http://localhost:1337/me-api/graphql';
 
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
     return {
